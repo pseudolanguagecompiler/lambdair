33f88faafe0a0c4c5d6c8f935d0a5e84a05e53be
LQ <234985631+pseudolanguagecompiler@users.noreply.github.com>
2025-12-04 17:36:50 -0500

    Update Base.lean

--- Ast/Base.lean
+++ Ast/Base.lean
@@ -1,7 +1,11 @@
--- Ast/Base.lean
+namespace Ast
+
 class IR where
   Expr : Type
   Stmt : Type
   stateType : Type
   evalExpr : Expr → stateType → Nat
   evalStmt : Stmt → stateType → stateType
+  evalProgram : List Stmt → stateType → IO Nat
+
+end Ast
